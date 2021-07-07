6163d1bdc1b3ae9127707a4c4083c384bb0e5ddc
sseagull <54673844+sseagull@users.noreply.github.com>
2021-07-07 16:54:58 -0400

    feat: added a exists function to zshenv

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,8 +2,13 @@
 
 echo '\n<<< Starting Homebrew Setup >>>\n'
 
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+  echo "brew exists, skipping the install"
+else 
+  echo "brew doesn't exist, installing"
+  # Install homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Install brew contents using Brewfile
 brew bundle --verbose
